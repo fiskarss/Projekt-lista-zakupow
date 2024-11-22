22c824322223a8dfbe339c9a0ed4fd9adbda9ea3
fiskarss <milosz.sanetra@o2.pl>
2024-11-22 16:38:19 +0100

    Update Class1.cs

--- src/Shared/Domain/Entities/Class1.cs
+++ src/Shared/Domain/Entities/Class1.cs
@@ -1,10 +1,37 @@
-﻿namespace projekt.models
+public class Product
 {
-    public class Product
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Count { get; set; }
+    public double Price { get; set; }
+}
+
+
+namespace System.Linq
+{
+
+}
+
+namespace GFG
+{
+
+    class Program
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int Count { get; set; }
-        public double Price { get; set; }
+
+        static void Main(string[] args)
+        {
+
+            // Display current Foreground color 
+            Console.WriteLine("Default Foreground Color: {0}",
+                                     Console.ForegroundColor);
+
+            // Set the Foreground color to blue 
+            Console.ForegroundColor
+                = ConsoleColor.Blue;
+
+            // Display current Foreground color 
+            Console.WriteLine("Changed Foreground Color: {0}",
+                                    Console.ForegroundColor);
+        }
     }
 }
